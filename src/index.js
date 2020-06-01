1c7bc2578631afc0f103223e70a5c2743fb98659
cangyu1993 <1765244720@qq.com>
2020-06-01 16:51:11 +0800

    打包二级目录

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom'
 import Router from './router'
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/pwbweb'>
         <App>
             <Router/>
         </App>
